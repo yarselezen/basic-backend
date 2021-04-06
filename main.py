630327a04931250150696f4f439dac7a197ddb27
kisliakovsky <ilya.kisliakovskii@razz.team>
2021-04-06 22:43:22 +0300

    lab-58477786 [main] Validate command line args

--- main.py
+++ main.py
@@ -5,15 +5,23 @@ from bottle import Bottle
 
 from project.app.app import App
 
+allowed_hosts = {'localhost', '127.0.0.1', '0.0.0.0'}
+
 
 def main():
     host = 'localhost'
-    port = 8080
-    if len(sys.argv) > 2:
+    port = '8080'
+    arg_len = len(sys.argv)
+    if arg_len == 2:
+        port = sys.argv[1]
+    if arg_len > 2:
         host = sys.argv[1]
-        port = int(sys.argv[2])
-    db_path = Path('./db/main.db')
-    App(Bottle(autojson=False), host, port, db_path).launch()
+        port = sys.argv[2]
+    if host not in allowed_hosts:
+        raise ValueError("Host '{}' is not allowed".format(host))
+    if not port.isdigit():
+        raise ValueError("Port '{}' is invalid".format(port))
+    App(Bottle(autojson=False), host, int(port), Path('./db/main.db')).launch()
 
 
 main()
